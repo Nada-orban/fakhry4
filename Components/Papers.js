5741f6d6280c57395c5ac05731356e601bedb61b
Ahmed Fakhry <44531149+AhmedFakhry47@users.noreply.github.com>
2024-07-25 00:46:26 +0900

    Update Papers.js

--- Components/Papers.js
+++ Components/Papers.js
@@ -11,7 +11,7 @@ function Papers() {
     const colorMode = React.useContext(ColorModeContext);
     const theme = useTheme();
     return (
-        <Box id="papers" py="90px" textAlign="center" backgroundColor="background.secondary" color="white">
+        <Box id="papers" py="90px" textAlign="center" backgroundColor="background.secondary" color="black">
             <Container>
                 <Typography variant='h2' sx={{ textAlign: "center", fontWeight: "bold" }}>Papers</Typography>
                 <Box my="40px">
